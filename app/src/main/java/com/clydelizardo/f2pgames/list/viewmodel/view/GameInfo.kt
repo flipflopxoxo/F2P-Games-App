468e3a3ff2dcd3266caacd83ce1e46ca9284935f
Clyde <box.magnet@gmail.com>
2021-07-15 20:27:55 +0100

    Create initial GameInfo model

---
+++ app/src/main/java/com/clydelizardo/f2pgames/list/viewmodel/view/GameInfo.kt
@@ -0,0 +1,12 @@
+package com.clydelizardo.f2pgames.list.viewmodel.view
+
+import java.util.*
+
+data class GameInfo(
+    val name: String,
+    val description: String,
+    val releaseDate: Date,
+    val genre: String,
+    val platform: String,
+    val link: String
+)
