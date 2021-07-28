92b35d77bd38dba7c35775eadda66a18d7a9de5f
Clyde Lizardo <clyde.lizardo@gmail.com>
2021-07-28 06:16:58 +0100

    Bind main activity to GameListViewModel

--- app/src/main/java/com/clydelizardo/f2pgames/MainActivity.kt
+++ app/src/main/java/com/clydelizardo/f2pgames/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.clydelizardo.f2pgames
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.clydelizardo.f2pgames.databinding.ActivityMainBinding
+import com.clydelizardo.f2pgames.di.core.DaggerAppComponent
+import com.clydelizardo.f2pgames.list.view.GameListAdapter
+import com.clydelizardo.f2pgames.list.viewmodel.GameListViewModel
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DaggerAppComponent.builder()
+            .build()
+            .inject(this)
+        val viewModel: GameListViewModel by viewModels {
+            viewModelFactory
+        }
+
         val view = ActivityMainBinding.inflate(layoutInflater)
         setContentView(view.root)
+        view.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val gameListAdapter = GameListAdapter()
+        view.recyclerView.adapter = gameListAdapter
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.start()
+        }
+
+        lifecycleScope.launchWhenResumed {
+            viewModel.listOfGames.collect {
+                gameListAdapter.submitList(it)
+            }
+        }
     }
 }
