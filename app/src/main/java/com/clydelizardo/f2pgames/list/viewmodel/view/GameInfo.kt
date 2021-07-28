9dd2882c9b8071554f57fed1a66b5c9d61bb238e
Clyde Lizardo <clyde.lizardo@gmail.com>
2021-07-28 06:15:40 +0100

    Change field type for GameInfo releaseDate to nullable

--- app/src/main/java/com/clydelizardo/f2pgames/list/viewmodel/view/GameInfo.kt
+++ app/src/main/java/com/clydelizardo/f2pgames/list/viewmodel/view/GameInfo.kt
@@ -6,7 +6,7 @@ data class GameInfo(
     val id: String,
     val name: String,
     val description: String,
-    val releaseDate: Date,
+    val releaseDate: Date?,
     val genre: String,
     val platform: String,
     val link: String,
