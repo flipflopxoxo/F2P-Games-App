a2f5163f5162ba9f0cfee7232f3eb0f3ec2fc536
Clyde Lizardo <clyde.lizardo@gmail.com>
2021-08-05 22:23:50 +0100

    Enable long press on game list item

--- app/src/main/java/com/clydelizardo/f2pgames/list/view/GameListAdapter.kt
+++ app/src/main/java/com/clydelizardo/f2pgames/list/view/GameListAdapter.kt
@@ -8,26 +8,29 @@ import com.clydelizardo.f2pgames.databinding.ListItemGameInfoBinding
 import com.clydelizardo.f2pgames.list.view.viewholder.GameEntryViewHolder
 import com.clydelizardo.f2pgames.model.GameInfo
 
-class GameListAdapter : ListAdapter<GameInfo, GameEntryViewHolder>(object :
-    DiffUtil.ItemCallback<GameInfo>() {
-    override fun areItemsTheSame(oldItem: GameInfo, newItem: GameInfo) = oldItem.id == newItem.id
-
-    override fun areContentsTheSame(oldItem: GameInfo, newItem: GameInfo) =
-        oldItem == newItem.copy(id = oldItem.id)
-
-}) {
+class GameListAdapter : ListAdapter<GameInfo, GameEntryViewHolder>(GameInfoItemCallback) {
     var selectionListener: OnGameSelected? = null
+    var longPressListener: LongPressListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameEntryViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding =
             ListItemGameInfoBinding.inflate(layoutInflater, parent, false)
-        return GameEntryViewHolder(binding) {
+        return GameEntryViewHolder(binding, {
             selectionListener?.onGameSelected(it)
+        }) {
+            longPressListener?.onLongPress(it)
         }
     }
 
     override fun onBindViewHolder(holder: GameEntryViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+}
+
+object GameInfoItemCallback : DiffUtil.ItemCallback<GameInfo>() {
+    override fun areItemsTheSame(oldItem: GameInfo, newItem: GameInfo) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: GameInfo, newItem: GameInfo) =
+        oldItem == newItem.copy(id = oldItem.id)
 }
